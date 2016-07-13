d88eda13fcc057c5b4309bf3cdf8c8947bbb71e3
rferroni <rferroni@gmail.com>
2016-07-13 12:40:35 +0200

    Update create_dovpn.sh

--- create_dovpn.sh
+++ create_dovpn.sh
@@ -14,19 +14,20 @@ if [[ $# -eq 0 ]]
 	then 
 	     	echo $1
 		# create directory for the new instance
-		cp -R $HOME/0000/ $HOME/$1
+		cp -R $HOME/DoVPN/0000/ $HOME/$1
 
 		# create new instance of dovpn
-		docker run -ti -d -P --privileged --name dovpn$1 -e PORT="port ${1}" -e REMOTE="remote dovpn.ml ${1}" -p $1:$1/udp --net=bridge -v $HOME/$1:/etc/openvpn:rw rferroni/dovpn /bin/bash
+		docker run -ti -d -P --privileged --name dovpn$1 -e PORT="port ${1}" -e REMOTE="remote dovpn.ml ${1}" -p $1:$1/udp --net=bridge -v $HOME/DoVPN/$1:/etc/openvpn:rw rferroni/dovpn /bin/bash
 		sleep 3
 
 		# inicialice cert & ovpn
 		docker exec dovpn$1 /bin/bash -c /etc/openvpn/initialize.sh
 
-		# copy client.ovpn
-		mkdir /var/nginx/dovpn/config/$1
-		cp $HOME/client$1.ovpn /var/nginx/dovpn/config/client$1.ovpn
-
+		# copy client.ovpn 
+			# if you have a web server you can access later with an URL
+			# Example: https://dovpn.ml/client1194.ovpn
+			cp $HOME/client$1.ovpn /var/nginx/dovpn/client$1.ovpn
+		
 	else
 		echo "The port number have to be between 1194 and 1204"
 		echo "Example: ./create_dovpn.sh 1194"
